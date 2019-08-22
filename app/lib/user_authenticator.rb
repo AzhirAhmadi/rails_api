5091c3f1739bb5929363177ac58febcb0cd2420e
AzhirAhmadi <aaahmadi99@gmail.com>
2019-08-22 19:03:19 +0430

    end of video #17

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -3,10 +3,25 @@ class UserAuthenticator
     attr_reader :user
 
     def initialize(code)
-
+        @code = code
     end
 
     def perform
-        raise AuthenticationError
+        client = Octokit::Client.new(
+            client_id: "836b314d9f71e85a5f14",
+            client_secret: "dc2528e56f5e8f95fd07399cfab70e4680d67abb"
+        )
+        token = client.exchange_code_for_token(code)
+        if token.try(:error).present?
+            raise AuthenticationError
+        else
+            user_client = Octokit::Client.new(access_token: token)
+            user_data = user_client.user.to_h.slice(:login, :avatar_url, :url, :name)
+            User.create(user_data.merge(provider: "github"))
+
+        end
     end
+
+    private
+        attr_reader :code
 end
