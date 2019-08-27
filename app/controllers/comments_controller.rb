c34c1c05e53795e7a9786fdde98451bee9747e63
AzhirAhmadi <aaahmadi99@gmail.com>
2019-08-27 17:27:15 +0430

    enf of video #36

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,24 @@
 class CommentsController < ApplicationController
   skip_before_action :authorize!, only: [:index]
-  before_action :load_article, only: [:create]
+  
   def index
-    @comments = Comment.all
-
-    render json: @comments
+    render json: article.comments.page(params[:page]).per(params[:per_page])
   end
 
   def create
-    @comment = @article.comments.build(comment_params.merge(user: current_user))
+    comment = article.comments.build(comment_params.merge(user: current_user))
 
-    if @comment.save
-      render json: @comment, status: :created, location: @article
+    if comment.save
+      render json: comment, status: :created, location: article
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json: comment.errors, status: :unprocessable_entity
     end
+  end
+  def article
+    article = Article.find(params[:article_id])
   end
 
   private
-    def load_article
-      @article = Article.find(params[:article_id])
-    end
-
     def comment_params
       params.require(:comment).permit(:content)
     end
