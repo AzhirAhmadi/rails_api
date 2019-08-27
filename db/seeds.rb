2269d2d60afa8997973141d9c9566ef63b023fb8
AzhirAhmadi <aaahmadi99@gmail.com>
2019-08-27 19:05:42 +0430

    edit db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create({
+    login: "jsmith", name: "John Smith", provider: "github"
+})
+
 Article.create([
-    {title: "Title", content: "Content", slug: "Slug-01"},
-    {title: "Title", content: "Content", slug: "Slug-02"},
-    {title: "Title", content: "Content", slug: "Slug-03"},
-    {title: "Title", content: "Content", slug: "Slug-04"},
-    {title: "Title", content: "Content", slug: "Slug-05"},
-    {title: "Title", content: "Content", slug: "Slug-06"},
-    {title: "Title", content: "Content", slug: "Slug-07"},
-    {title: "Title", content: "Content", slug: "Slug-08"},
-    {title: "Title", content: "Content", slug: "Slug-09"},
-    {title: "Title", content: "Content", slug: "Slug-10"},
-    {title: "Title", content: "Content", slug: "Slug-11"},
-    {title: "Title", content: "Content", slug: "Slug-12"},
-    {title: "Title", content: "Content", slug: "Slug-13"},
-    {title: "Title", content: "Content", slug: "Slug-14"},
-    {title: "Title", content: "Content", slug: "Slug-15"}
+    {title: "Title", content: "Content", slug: "Slug-01", user: user},
+    {title: "Title", content: "Content", slug: "Slug-02", user: user},
+    {title: "Title", content: "Content", slug: "Slug-03", user: user},
+    {title: "Title", content: "Content", slug: "Slug-04", user: user},
+    {title: "Title", content: "Content", slug: "Slug-05", user: user},
+    {title: "Title", content: "Content", slug: "Slug-06", user: user},
+    {title: "Title", content: "Content", slug: "Slug-07", user: user},
+    {title: "Title", content: "Content", slug: "Slug-08", user: user},
+    {title: "Title", content: "Content", slug: "Slug-09", user: user},
+    {title: "Title", content: "Content", slug: "Slug-10", user: user},
+    {title: "Title", content: "Content", slug: "Slug-11", user: user},
+    {title: "Title", content: "Content", slug: "Slug-12", user: user},
+    {title: "Title", content: "Content", slug: "Slug-13", user: user},
+    {title: "Title", content: "Content", slug: "Slug-14", user: user},
+    {title: "Title", content: "Content", slug: "Slug-15", user: user}
 ])
