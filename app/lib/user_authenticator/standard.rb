c703a6b1e9e6ad0bb0f0b592a9d1045c4ac7fcaa
AzhirAhmadi <aaahmadi99@gmail.com>
2019-08-29 10:54:27 +0430

    end of video #46

--- app/lib/user_authenticator/standard.rb
+++ app/lib/user_authenticator/standard.rb
@@ -2,10 +2,20 @@ class UserAuthenticator::Standard < UserAuthenticator
     attr_reader :user, :access_token
     
     def initialize(login, password)
-
+        @login = login
+        @password =password
     end
     
     def perform
-        raise ErrorHandeler::AuthenticationError::Standard
+        raise ErrorHandeler::AuthenticationError::Standard if (login.blank? || password.blank?)
+        raise ErrorHandeler::AuthenticationError::Standard unless User.exists?(login: login)
+
+        user =User.find_by(login: login)
+
+        raise ErrorHandeler::AuthenticationError::Standard unless user.password == password
     end
+
+    private
+
+        attr_reader :login, :password
 end
--- spec/lib/user_authenticator/standard_spec.rb
+++ spec/lib/user_authenticator/standard_spec.rb
@@ -26,7 +26,19 @@ describe UserAuthenticator::Standard do
         end
 
         context "when successed auth" do
+            let(:user) { create :user, login: "jsmith", password: "password"}
 
+            {user}
+            
+            it "should set the user found in db" do
+                expect { subject }.not_to change{ User.count }
+                expect { authenticator.user }.to eq(user)
+            end
+
+            it "should create and set user access token" do
+                expect{ subject }.to change{ AccessToken.count }.by(1)
+                expect(authenticator.access_token).to be_present
+            end
         end 
     end
 end
